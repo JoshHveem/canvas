64e078052ff5f1140cde8ca4f92a3a645664b0eb
Joshua Hveem <Josh.Hveem@btech.edu>
2025-06-06 08:37:27 -0600

    add course column

--- custom_features/reports/grades_page/template.vue
+++ custom_features/reports/grades_page/template.vue
@@ -28,7 +28,7 @@
             style="
               padding: .25rem .5rem;
               display: grid;
-              grid-template-columns: auto 10rem 4.5rem 4.5rem 10rem 4rem 4rem 4rem;
+              grid-template-columns: auto 10rem 10rem 4.5rem 4.5rem 10rem 4rem 4rem 4rem;
               align-items: center;
               font-size: 0.75rem;
               cursor: help;
@@ -78,7 +78,7 @@
               style="
                 padding: .25rem .5rem;
                 display: grid;
-                grid-template-columns: auto 10rem 4.5rem 4.5rem 10rem 4rem 4rem 4rem;
+                grid-template-columns: auto 10rem 10rem 4.5rem 4.5rem 10rem 4rem 4rem 4rem;
                 align-items: center;
                 font-size: 0.75rem;
               "
