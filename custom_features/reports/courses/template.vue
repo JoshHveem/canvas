f0bc6185e145fc749d46d3c0d84c864bea4411f7
Joshua Hveem <Josh.Hveem@btech.edu>
2025-07-08 09:51:24 -0600

    courses reports

--- custom_features/reports/courses/template.vue
+++ custom_features/reports/courses/template.vue
@@ -1,3 +1,26 @@
 <template>
-    <div>test</div>
+    <div class='btech-modal btech-canvas-report' style='display: inline-block;'>
+        <div class='btech-modal-content'>
+            <div class='btech-modal-content-inner'>
+                <span class='btech-close' v-on:click='close()'>&times;</span>
+                <h3 style='text-align: center;'>Report</h3>
+                <div
+                style="display: inline-block;"
+                v-if="!courseId"
+                >
+                <label>Course Source</label>
+                <select 
+                    v-model="settings.account" 
+                    @change="saveSettings(settings); 
+                    loadCourseEnrollments();"
+                    >
+                    <option v-for="account in accounts" :value="account.id">{{ account.name }}</option>
+                </select>
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
