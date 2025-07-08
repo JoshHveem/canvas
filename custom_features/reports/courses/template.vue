9b43854203dbb8ee1f291681c52566659d4c1569
Joshua Hveem <Josh.Hveem@btech.edu>
2025-07-08 09:52:13 -0600

    courses reports

--- custom_features/reports/courses/template.vue
+++ custom_features/reports/courses/template.vue
@@ -5,17 +5,16 @@
                 <span class='btech-close' v-on:click='close()'>&times;</span>
                 <h3 style='text-align: center;'>Report</h3>
                 <div
-                style="display: inline-block;"
-                v-if="!courseId"
+                    style="display: inline-block;"
                 >
-                <label>Course Source</label>
-                <select 
-                    v-model="settings.account" 
-                    @change="saveSettings(settings); 
-                    loadCourseEnrollments();"
-                    >
-                    <option v-for="account in accounts" :value="account.id">{{ account.name }}</option>
-                </select>
+                    <label>Course Source</label>
+                    <select 
+                        v-model="settings.account" 
+                        @change="saveSettings(settings); 
+                        loadCourseEnrollments();"
+                        >
+                        <option v-for="account in accounts" :value="account.id">{{ account.name }}</option>
+                    </select>
                 </div>
                 <div>
 
