4ffff40a649cab5e5397862f108ccbb48bc7d269
Joshua Hveem <Josh.Hveem@btech.edu>
2025-07-09 08:42:56 -0600

    cling

--- custom_features/reports/courses/report.js
+++ custom_features/reports/courses/report.js
@@ -274,7 +274,7 @@
                 tagList.sort((a, b) => b.cnt - a.cnt);
 
                 for (let { tag, cnt, perc } of tagList) {
-                  str += `<span title="${cnt} (${(perc * 100).toFixed(1)}%) responses" class="btech-pill-text" style="white-space: nowrap; margin-right: 0.25rem; background-color: ${
+                  str += `<span title="${cnt} (${(perc * 100).toFixed(1)}%) responses" class="btech-pill-text" style="white-space: nowrap; font-size: 0.75rem; margin-right: 0.25rem; background-color: ${
                     perc > 0.30 ? this.colors.red : (perc > 0.15 ? this.colors.orange : this.colors.yellow)
                   }; color: white;">${tag}</span>`;
                 }
