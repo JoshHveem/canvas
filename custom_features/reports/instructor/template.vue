2a12c5a1233fbe1d416dd42b63c823c3bb035a15
Joshua Hveem <Josh.Hveem@btech.edu>
2025-08-12 15:57:20 -0600

    add instructor data

--- custom_features/reports/instructor/template.vue
+++ custom_features/reports/instructor/template.vue
@@ -39,53 +39,6 @@
                 >
                 </div>
                 <div>
-                    <div
-                        style="
-                        padding: .25rem .5rem;
-                        display: grid;
-                        align-items: center;
-                        font-size: 0.75rem;
-                        cursor: help;
-                        user-select: none;
-                        "
-                        :style="{
-                        'grid-template-columns': getColumnsWidthsString()
-                        }"
-                    >
-                        <div 
-                            v-for='column in visibleColumns' 
-                            style="display: inline-block; grid-template-columns: auto 1rem;"
-                            :key='column.name' 
-                            :title='column.description'
-                            @click=" setSortColumn(column.name);;"
-                        >
-                        <span><b>{{column.name}}</b></span>
-                        <span>
-                            <svg 
-                            style="width: .75rem; height: .75rem;" 
-                            x="0px" 
-                            y="0px"
-                            viewBox="0 0 490 490" 
-                            style="enable-background:new 0 0 490 490;" 
-                            xml:space="preserve">
-                            <g>
-                            <polygon 
-                                :style="{
-                                'fill': column.sort_state < 0 ? '#000000' : '#E0E0E0'
-                                }"
-                                points="85.877,154.014 85.877,428.309 131.706,428.309 131.706,154.014 180.497,221.213 217.584,194.27 108.792,44.46 
-                                0,194.27 37.087,221.213 	"/>
-                            <polygon 
-                                :style="{
-                                'fill': column.sort_state > 0 ? '#000000' : '#E0E0E0'
-                                }"
-                                points="404.13,335.988 404.13,61.691 358.301,61.691 358.301,335.99 309.503,268.787 272.416,295.73 381.216,445.54 
-                                490,295.715 452.913,268.802 	"/>
-                            </g>
-                            </svg>
-                        </span>
-                        </div>
-                    </div>
                     <div>
                     </div>
                 </div>
