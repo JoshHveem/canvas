014bfbb0a7d85c7f42c8c070b97566973911de9f
Joshua Hveem <Josh.Hveem@btech.edu>
2025-09-10 10:30:38 -0600

    force looking at specific user

--- custom_features/reports/instructor/report.js
+++ custom_features/reports/instructor/report.js
@@ -108,7 +108,7 @@
           const base = {
             year: this.settings.filters.year,
             account: this.settings.account,
-            instructorId: ENV.current_user_id  // optional; child can use or ignore
+            instructorId: 1840071 // ENV.current_user_id  // optional; child can use or ignore
           };
           return base; // other reports can use what they need from base
         },
