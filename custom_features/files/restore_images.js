8890ff8300d323e8b009db06fd8fe8c1acb06844
Joshua Hveem <Josh.Hveem@btech.edu>
2025-10-01 09:08:59 -0600

    check files

--- custom_features/files/restore_images.js
+++ custom_features/files/restore_images.js
@@ -1,32 +1,36 @@
 $(async function () {
-  $("img").each(async function () {
-    const img = $(this);
-    const src = img.attr("src");
+  async function checkTag(tagType) {
+    $(tagType).each(async function () {
+      const img = $(this);
+      const src = img.attr("src");
 
-    // Match Canvas preview image URL: /courses/{course_id}/files/{file_id}/preview
-    const match = src.match(/\/courses\/(\d+)\/files\/(\d+)/);
+      // Match Canvas preview image URL: /courses/{course_id}/files/{file_id}/preview
+      const match = src.match(/\/courses\/(\d+)\/files\/(\d+)/);
 
-    if (match) {
-      const courseId = match[1];
-      const fileId = match[2];
-      try {
-        let file = await canvasGet(`/api/v1/courses/${courseId}/files/${fileId}`);
-      } catch (err) {
-        const restoreBtn = $(`
-          <button style="margin:5px; background:#ffd; border:1px solid #ccc;" class="canvas-restore-btn" data-course-id="${courseId}" data-file-id="${fileId}">
-            🔄 Restore Image
-          </button>
-        `);
+      if (match) {
+        const courseId = match[1];
+        const fileId = match[2];
+        try {
+          let file = await canvasGet(`/api/v1/courses/${courseId}/files/${fileId}`);
+        } catch (err) {
+          const restoreBtn = $(`
+            <button style="margin:5px; background:#ffd; border:1px solid #ccc;" class="canvas-restore-btn" data-course-id="${courseId}" data-file-id="${fileId}">
+              🔄 Restore Image
+            </button>
+          `);
 
-        img.after(restoreBtn);
+          img.after(restoreBtn);
 
-        restoreBtn.on("click", function () {
-          const courseId = $(this).data("course-id");
-          const fileId = $(this).data("file-id");
+          restoreBtn.on("click", function () {
+            const courseId = $(this).data("course-id");
+            const fileId = $(this).data("file-id");
 
-          $.post(`https://btech.instructure.com/courses/${courseId}/undelete/attachment_${fileId}`);
-        });
+            $.post(`https://btech.instructure.com/courses/${courseId}/undelete/attachment_${fileId}`);
+          });
+        }
       }
-    }
-  });
+    });
+  }
+  checkTag("img");
+  checkTag("a");
 });
