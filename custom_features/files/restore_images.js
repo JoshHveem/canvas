42ba88b184fe2e34835e10b3a436c8287baa6537
Joshua Hveem <Josh.Hveem@btech.edu>
2025-10-01 09:21:07 -0600

    check files

--- custom_features/files/restore_images.js
+++ custom_features/files/restore_images.js
@@ -1,8 +1,14 @@
+/*
+  There is not currently a simple way for instructors to restore deleted files.
+  This is a workaround that locates all of a given element (located through a query string) and checks a given parameter for a file url
+  If it finds a link to a Canvas file, it will check if it is pullable using the api. Currently the api throws an error if a file is deleted.
+  If it throws an error, ad a button that instructors can click to restore that file. Refresh.
+*/
 $(async function () {
-  async function checkFileEl(elLocator, linkParam) {
+  async function checkFileEl(elLocator, linkAttr) {
     $(elLocator).each(async function () {
-      const img = $(this);
-      const src = img.attr(linkParam);
+      const el = $(this);
+      const src = el.attr(linkAttr);
 
       // Match Canvas preview image URL: /courses/{course_id}/files/{file_id}/preview
       const match = src.match(/\/courses\/(\d+)\/files\/(\d+)/);
@@ -19,14 +25,24 @@ $(async function () {
             </button>
           `);
 
-          img.after(restoreBtn);
+          el.after(restoreBtn);
 
-          restoreBtn.on("click", function () {
+          restoreBtn.on("click", async function () {
             const courseId = $(this).data("course-id");
             const fileId = $(this).data("file-id");
 
-            $.post(`https://btech.instructure.com/courses/${courseId}/undelete/attachment_${fileId}`);
+            // Undelete request
+            await $.post(`https://btech.instructure.com/courses/${courseId}/undelete/attachment_${fileId}`);
+
+            // Force element to reload its file
+            const oldUrl = el.attr(linkAttr);
+            const newUrl = oldUrl + (oldUrl.includes("?") ? "&" : "?") + "t=" + Date.now();
+            el.attr(linkAttr, newUrl);
+
+            // Remove the restore button once done
+            $(this).remove();
           });
+
         }
       }
     });
