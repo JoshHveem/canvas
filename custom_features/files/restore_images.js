9e80e8d5ed98271565bc324536e160d9968eaf75
Joshua Hveem <Josh.Hveem@btech.edu>
2025-10-01 08:36:14 -0600

    check files

--- custom_features/files/restore_images.js
+++ custom_features/files/restore_images.js
@@ -9,28 +9,25 @@ $(async function () {
     if (match) {
       const courseId = match[1];
       const fileId = match[2];
-      let file = await canvasGet(`/api/v1/courses/${courseId}/files/${fileId}`);
-      console.log(file);
+      try {
+        let file = await canvasGet(`/api/v1/courses/${courseId}/files/${fileId}`);
+        console.log(file);
+      } catch (err) {
+        const restoreBtn = $(`
+          <button style="margin:5px; background:#ffd; border:1px solid #ccc;" class="canvas-restore-btn" data-course-id="${courseId}" data-file-id="${fileId}">
+            🔄 Restore Image
+          </button>
+        `);
 
-      // Check if the image is broken
-      $("<img>")
-        .attr("src", src)
-        .on("error", function () {
-          const restoreBtn = $(`
-            <button style="margin:5px; background:#ffd; border:1px solid #ccc;" class="canvas-restore-btn" data-course-id="${courseId}" data-file-id="${fileId}">
-              🔄 Restore Image
-            </button>
-          `);
+        img.after(restoreBtn);
 
-          img.after(restoreBtn);
+        restoreBtn.on("click", function () {
+          const courseId = $(this).data("course-id");
+          const fileId = $(this).data("file-id");
 
-          restoreBtn.on("click", function () {
-            const courseId = $(this).data("course-id");
-            const fileId = $(this).data("file-id");
-
-            $.post(`https://btech.instructure.com/courses/${courseId}/undelete/attachment_${fileId}`);
-          });
+          $.post(`https://btech.instructure.com/courses/${courseId}/undelete/attachment_${fileId}`);
         });
+      }
     }
   });
 });
