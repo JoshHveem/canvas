870c29b6abe59357ecb2cc8564d4b9ed05200124
Joshua Hveem <Josh.Hveem@btech.edu>
2025-11-17 11:44:07 -0700

    cleaning up grades between dates

--- custom_canvas.js
+++ custom_canvas.js
@@ -282,20 +282,19 @@ var MONTH_NAMES_SHORT = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug
       feature("reports/grades_page/report", /^\/courses\/[0-9]+\/gradebook$/);
       feature("hs/enroll", /^\/accounts\/[0-9]+\/enrollhs$/);
       if (IS_ME) {
-        feature("reports/individual_page/report", [
-          /^\/courses\/[0-9]+\/users\/[0-9]+$/,
-          /^\/accounts\/[0-9]+\/users\/[0-9]+$/,
-          /^\/users\/[0-9]+$/,
-          /^\/courses\/[0-9]+\/grades\/[0-9]+$/
-        ]);
-      } else {
-        feature("reports/individual_page/report", [
+        feature("reports/individual_page/report2", [
           /^\/courses\/[0-9]+\/users\/[0-9]+$/,
           /^\/accounts\/[0-9]+\/users\/[0-9]+$/,
           /^\/users\/[0-9]+$/,
           /^\/courses\/[0-9]+\/grades\/[0-9]+$/
         ]);
       }
+      feature("reports/individual_page/report", [
+        /^\/courses\/[0-9]+\/users\/[0-9]+$/,
+        /^\/accounts\/[0-9]+\/users\/[0-9]+$/,
+        /^\/users\/[0-9]+$/,
+        /^\/courses\/[0-9]+\/grades\/[0-9]+$/
+      ]);
     }
     feature("password_reset", [
       /^\/courses\/[0-9]+\/users\/[0-9]+$/,
