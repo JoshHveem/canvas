d33458e62e1c3ec7b5dbe606c192f98fe0265f89
Joshua Hveem <Josh.Hveem@btech.edu>
2026-01-15 15:30:23 -0700

    reports

--- custom_features/side_menus.js
+++ custom_features/side_menus.js
@@ -57,7 +57,7 @@
     )
     createSideMenuButton(
       'Automations',
-      'https://btech.instructore.com/automations',
+      'https://btech.instructure.com/automations',
       `<svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 48 48"
